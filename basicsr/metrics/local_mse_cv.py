507999cfc7c3dc89316fb889c0ababa03b8f1901
Magda Radić <magdina.radic@gmail.com>
2025-06-19 22:55:00 +0200

    change local mse std to local mse cv

--- basicsr/metrics/local_mse_cv.py
+++ basicsr/metrics/local_mse_cv.py
@@ -3,13 +3,14 @@ import torch
 import torch.nn.functional as F
 from basicsr.metrics.metric_util import reorder_image, to_y_channel
 
-def calculate_local_mse_std(img1,
-                            img2,
-                            crop_border,
-                            input_order='HWC',
-                            test_y_channel=False,
-                            pool_size=32):
-    """Calculate the standard deviation of local MSEs between two images.
+
+def calculate_local_mse_cv(img1,
+                           img2,
+                           crop_border,
+                           input_order='HWC',
+                           test_y_channel=False,
+                           pool_size=32):
+    """Calculate coefficient of variation (CV) of local MSEs between two images.
 
     Args:
         img1 (ndarray/tensor): Image with range [0, 255]/[0, 1].
@@ -20,7 +21,7 @@ def calculate_local_mse_std(img1,
         pool_size (int): Pool size for local MSE. Default: 32.
 
     Returns:
-        float: Standard deviation of local MSEs.
+        float: CV (std/mean) of local MSEs.
     """
     assert img1.shape == img2.shape, (
         f'Image shapes are different: {img1.shape}, {img2.shape}.')
@@ -31,11 +32,11 @@ def calculate_local_mse_std(img1,
     if isinstance(img1, torch.Tensor):
         if len(img1.shape) == 4:
             img1 = img1.squeeze(0)
-        img1 = img1.detach().cpu().numpy().transpose(1, 2, 0)
+        img1 = img1.detach().cpu().numpy().transpose(1, 2, 0) if input_order == 'CHW' else img1.detach().cpu().numpy()
     if isinstance(img2, torch.Tensor):
         if len(img2.shape) == 4:
             img2 = img2.squeeze(0)
-        img2 = img2.detach().cpu().numpy().transpose(1, 2, 0)
+        img2 = img2.detach().cpu().numpy().transpose(1, 2, 0) if input_order == 'CHW' else img2.detach().cpu().numpy()
 
     # Reorder to HWC, float64
     img1 = reorder_image(img1, input_order=input_order).astype(np.float64)
@@ -72,6 +73,12 @@ def calculate_local_mse_std(img1,
 
     # Pooling
     local_mse = F.avg_pool2d(mse_map, kernel_size=pool_size, stride=pool_size)
-    std_mse = local_mse.flatten().std().item()
+    local_mse_flat = local_mse.flatten()
 
-    return std_mse
+    mean_mse = local_mse_flat.mean().item()
+    std_mse = local_mse_flat.std().item()
+
+    if mean_mse == 0:
+        return 0.0
+
+    return std_mse / mean_mse
